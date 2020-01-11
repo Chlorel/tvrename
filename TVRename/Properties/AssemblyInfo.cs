e863911eb310b178eea7d5f8d5b72c54d45a3129
Chlorel <chlorel@gmail.com>
2020-01-11 15:59:56 +0100

    Update AssemblyInfo.cs

--- TVRename/Properties/AssemblyInfo.cs
+++ TVRename/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Resources;
 [assembly: Guid("3b5746c1-a5f7-48c2-a8de-95619489049b")]
 [assembly: NeutralResourcesLanguageAttribute("")]
 
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3")]
-[assembly: AssemblyInformationalVersion("3.0.3-rc7")] // Display version
+[assembly: AssemblyVersion("3.0.4.0")]
+[assembly: AssemblyFileVersion("3.0.4")]
+[assembly: AssemblyInformationalVersion("3.0.4")] // Display version
